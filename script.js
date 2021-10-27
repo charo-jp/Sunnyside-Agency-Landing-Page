584263b68fa0bd52a8ee0e75f81827aaa919c8ee
Yuya Hochi <yuya1charo@gmail.com>
2021-10-27 11:37:30 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function hamburger() {
+  let hamburgerButton = document.getElementById("hamburger");
+  hamburgerButton.addEventListener('click', () => {
+    let list = document.getElementById('list');
+    list.classList.toggle('changed');
+    
+  })
+
+}
+
+hamburger();
